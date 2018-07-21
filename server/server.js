c7a977e1fe88e17500eb765a80d93848aafd7638
ingleo <clgrhc@gmail.com>
2018-07-21 04:26:37 -0500

    validación cambios

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const createFile = require('./create-file/create-file');
 const ArbolBinario = require('./models/arbol-binario');
 
-//createFile.agregarNodos("70,84,85");
+createFile.agregarNodos("70,84,85");
 //createFile.agregarNodos("70,84,78,80");
 //createFile.agregarNodos("70,84,78,76");
 //createFile.agregarNodos("70,49,54,51");
 //createFile.agregarNodos("70,49,37,40");
-createFile.agregarNodos("70,49,37,22");
+//createFile.agregarNodos("70,49,37,22");
